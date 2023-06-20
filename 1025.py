8c8c225a600d9450726bb627b8ab476bb41a4071
hskimse <hskimse1@gmail.com>
2023-06-20 15:58:05 +0900

    1025 solved, y,x = i,j

--- 1025.py
+++ 1025.py
@@ -1,30 +1,23 @@
 from sys import stdin
 import math
+
 N, M = map(int, stdin.readline().split())
 arr = [stdin.readline().rstrip() for _ in range(N)]
-
-def sqtest(n):
-  return math.isqrt(n) ** 2 == n
-
-def filter(a,i,j,d1,d2):
-  result = 0
-  while i in range(0,N) and j in range(0,M) :
-    result *= 10
-    result += int(a[i][j])
-    if d1 == 0 and d2 == 0 :
-    	break
-    i += d1
-    j += d2
-  result = int(result)
-  return result
-
 res = -1
+
 for i in range(N):
   for j in range(M):
     for d1 in range(-N,N):
       for d2 in range(-M,M):
-        num = filter(arr,i,j,d1,d2)
-        if sqtest(num):
-          res = max(res,num)
+      	num = ''
+      	y, x = i, j
+      	while y in range(0,N) and x in range(0,M) :
+      		num += arr[y][x]
+      		if d1 == 0 and d2 == 0 :
+      			break
+      		if math.isqrt(int(num)) ** 2 == int(num):
+      			res = max(int(num),res)
+      		y += d1
+      		x += d2
 
 print(res)
