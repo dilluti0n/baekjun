d307e8ff2f92be4b9152b3968004c632ebb11604
hskimse <hskimse1@gmail.com>
2023-06-21 19:45:56 +0900

    1028.py

--- 1028.py
+++ 1028.py
@@ -3,3 +3,40 @@ from sys import stdin
 r, c = map(int, stdin.readline().split())
 arr = [stdin.readline().rstrip() for _ in range(r)]
 
+
+def diamove(n):
+	res = []
+	for i in range(n-1):
+		res.append((1,1))
+		res.append((1,-1))
+		res.append((-1,-1))
+		res.append((-1,1))
+	return res
+
+
+def check(arr,n,f):
+
+	if arr[f[0]][f[1]] == '0':
+		return False
+		
+	for move in diamove(n):
+		f[0] += move[0]
+		f[1] += move[1]
+		if f[0] < 0 or f[1] < 0 :
+			return False
+		
+		elif arr[f[0]][f[1]] == '0':
+			return False
+	
+	return True
+'''	
+for n in range(375):
+	for i in range(r):
+		for j in range(c):
+			f = [i,j]
+			if check(arr,n,f):
+				result = n
+
+print(result)
+'''
+print(arr)
