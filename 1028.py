2c9d6c834d089ab7a26ead893f3274c1642ddae2
hskimse <hskimse1@gmail.com>
2023-06-21 20:56:20 +0900

    1028 solved not qualified

--- 1028.py
+++ 1028.py
@@ -1,6 +1,5 @@
 from sys import stdin
 
-<<<<<<< HEAD
 r, c = map(int, stdin.readline().split())
 arr = [stdin.readline().rstrip() for _ in range(r)]
 
@@ -17,21 +16,25 @@ def diamove(n):
 
 def check(arr,n,f):
 
-	if arr[f[0]][f[1]] == '0':
-		return False
-		
-	for move in diamove(n):
-		f[0] += move[0]
-		f[1] += move[1]
-		if f[0] < 0 or f[1] < 0 :
-			return False
-		
-		elif arr[f[0]][f[1]] == '0':
-			return False
-	
-	return True
-'''	
-for n in range(375):
+    global r, c
+    if arr[f[0]][f[1]] == '0':
+        return False
+    
+    for move in diamove(n):
+        f[0] += move[0]
+        f[1] += move[1]
+        if f[0] < 0 or f[1] < 0 or f[0] > r-1 or f[1] > c-1:
+            return False
+        
+        elif arr[f[0]][f[1]] == '0':
+            return False
+        
+        else:
+        	return True
+
+result = 0
+
+for n in range(min(r//2+2,c//2+2)):
 	for i in range(r):
 		for j in range(c):
 			f = [i,j]
@@ -39,9 +42,3 @@ for n in range(375):
 				result = n
 
 print(result)
-'''
-print(arr)
-=======
-N, M = map(int, stdin.readline().split())
-arr = [stdin.readline().rstrip() for _ in range(N)]
->>>>>>> acfe04e3e1de2e0765a8e672c078b8f7830858a5
