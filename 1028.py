5b6b12f8d25d7c7136ee475ea12a38f331c5d625
hskimse <hskimse1@gmail.com>
2023-06-22 17:04:55 +0900

    trash

--- 1028.py
+++ 1028.py
@@ -5,7 +5,7 @@ r, c = map(int, stdin.readline().split())
 arr = [stdin.readline().rstrip() for _ in range(r)]
 '''
 
-r,c = 100, 100
+r,c = 175, 175
 arr = []
 for i in range(r):
     arr.append([])
@@ -13,40 +13,72 @@ for i in range(r):
         arr[i].append('1')
 
 start = time.time()
-def diamove(n):
-	res = []
-	for i in range(n-1):
-		res.append((1,1))
-	for i in range(n-1):
-		res.append((1,-1))
-	for i in range(n-1):
-		res.append((-1,-1))
-	for i in range(n-1):
-		res.append((-1,1))
-	return res
 
 
+def dp(arr,i,j,ind):
+
+	global r, c
+	cnt = 0
+	
+	while arr[i][j] != '0' and i < r and j < c and i > 0 and j > 0:
+		cnt += 1
+		i += -1 ** ind[0]
+		j += -1 ** ind[1]
+		
+	return cnt
+		
+
+'''
 def check(arr,n,f):
     global r, c
+    pseudo = []
+    
     if arr[f[0]][f[1]] == '0':
         return False
     
-    for move in diamove(n):
-        f[0] += move[0]
-        f[1] += move[1]
+    for i in range(n-1):
+        f[0] += 1
+        f[1] += 1
+        pseudo.append(arr[f[0]][f[1]])
         
+    if pseudo == [1] * (n-1):
+    	return True
+    	
         if arr[f[0]][f[1]] == '0':
             return False
+
     return True
+'''
 
 result = 0
+indl = [[2,2],[2,1],[1,1],[1,2]]
+temp = arr
 
-for n in range(1,min(r//2+2,c//2+2)):
-	for i in range(0,r-2*n+2):
-		for j in range(n-1,c-n+1):
-			f = [i,j]
-			if check(arr,n,f):
-				result = n
+for i in range(0,r):
+	for j in range(0,c):
+			temp[i][j] = [dp(arr,i,j,ind) for ind in indl]
+			
+def check(i,j):
+	mm = []
+	global m
+	for ind in range(4):
+		if temp[i][j][ind] == 0 or i < 0 or i >= c or j < 0 or j >= r:
+			return False
+			
+		else :
+			mm.append(temp[i][j][ind])
+			i += temp[i][j][ind]*(-1 ** indl[ind])
+			j += temp[i][j][ind]*(-1 ** indl[ind])
+		
+	if mm[0] == mm[1] and mm[1] == mm[2] and mm[2] == mm[3]:
+		m = mm[0]
+		return True
+			
+for i in range(0,r):
+	for j in range(0,c):
+		m = 0
+		if check:
+			result += m
 
 print(result)
 end = time.time()
